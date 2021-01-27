d3d84b8152120db43a6fe4464f1a617f82ad539b
Dom Ceccoli <domenic.ceccoli1@gmail.com>
2021-01-27 16:36:01 -0500

    Turning in Day 13 Inheritance Abstraction part 2 exercises

---
+++ module-1/13_Inheritance_Abstract_Classes/exercise-student/java/src/main/java/com/techelevator/SquareWall.java
@@ -0,0 +1,14 @@
+package com.techelevator;
+
+public class SquareWall extends RectangleWall {
+
+    //private int sideLength;
+
+    public SquareWall (String name, String color, int sideLength) {
+        super(name, color, sideLength, sideLength);
+    }
+
+    public String toString() {
+        return getName() + " (" + getHeight() + "x" + getLength() + ") " + "square";
+    }
+}
