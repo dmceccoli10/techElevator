a4a87a70966c30667c5d9d853e1fed22c359246a
Dom Ceccoli <domenic.ceccoli1@gmail.com>
2021-02-24 14:26:23 -0500

    turning in m02d13 homework serverside APIs

--- module-2/13_ServerSide_APIs_Part_1/exercise-student/java/src/main/java/com/techelevator/auctions/controller/AuctionController.java
+++ module-2/13_ServerSide_APIs_Part_1/exercise-student/java/src/main/java/com/techelevator/auctions/controller/AuctionController.java
@@ -17,4 +17,35 @@ public class AuctionController {
         this.dao = new MemoryAuctionDAO();
     }
 
+    //list method
+    @RequestMapping(method = RequestMethod.GET)
+    public List<Auction> list(@RequestParam(required = false) String title_like, @RequestParam(required = false) Double currentBid_lte) {
+        if (title_like != null && currentBid_lte != null) {
+         return dao.searchByTitleAndPrice(title_like, currentBid_lte);
+        }
+        if(title_like == null) {
+            title_like = "";
+        }
+        if(currentBid_lte == null) {
+            currentBid_lte = 0.00;
+        }
+        if(currentBid_lte > 0) {
+            return dao.searchByPrice(currentBid_lte);
+        }
+        return dao.searchByTitle(title_like);
+    }
+
+    //get action
+    @RequestMapping(path = "/{id}", method = RequestMethod.GET)
+    public Auction getAuctionById(@PathVariable int id) {
+        return dao.get(id);
+    }
+
+    //create action
+    @RequestMapping(method = RequestMethod.POST)
+    public Auction createAuction(@RequestBody Auction auction) {
+        return dao.create(auction);
+    }
+
+
 }
