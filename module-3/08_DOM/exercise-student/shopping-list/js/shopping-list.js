d78db0f2ef49a31d1c75c1f954286bdb51326a9e
Dom Ceccoli <domenic.ceccoli1@gmail.com>
2021-03-24 09:31:11 -0400

    turning in DOM homework, sorry I forgot to push

--- module-3/08_DOM/exercise-student/shopping-list/js/shopping-list.js
+++ module-3/08_DOM/exercise-student/shopping-list/js/shopping-list.js
@@ -1,23 +1,40 @@
 // add pageTitle
-
+const webpageTitle = 'My Shopping List';
 // add groceries
-
+const groceries = ['bananas', 'bread', 'milk', 'eggs', 'pasta', 'pasta sauce', 'cereal', 'waffles', 'syrup', 'pizza lunchables'];
 /**
  * This function will get a reference to the title and set its text to the value
  * of the pageTitle variable that was set above.
  */
-function setPageTitle() {}
+function setPageTitle() {
+  const pageTitle = document.getElementById('title');
+  pageTitle.innerText = webpageTitle;
+}
 
 /**
  * This function will loop over the array of groceries that was set above and add them to the DOM.
  */
-function displayGroceries() {}
+function displayGroceries() {
+  const groceryList = document.getElementById('groceries');
+  groceries.forEach(grocery => {
+    const listItem = document.createElement('li');
+    listItem.innerText = grocery;
+    groceryList.appendChild(listItem);
+  });
+}
 
 /**
  * This function will be called when the button is clicked. You will need to get a reference
  * to every list item and add the class completed to each one
  */
-function markCompleted() {}
+function markCompleted() {
+  const groceryList = document.getElementById('groceries');
+  groceryListItems = groceryList.getElementsByTagName('li');
+  for(let i = 0; i<groceryListItems.length; i++) {
+    item = groceryListItems[i];
+    item.classList.add('completed');
+  }
+}
 
 setPageTitle();
 
